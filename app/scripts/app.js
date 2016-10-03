979b249ce4940680a78ab5bf069f5a066c3d4388
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-10-03 18:11:30 -0500

    assignment-3-routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,12 +7,16 @@
          });
          $stateProvider
          .state('landing', {
-             url: '/',
+             url: '/landing',
              templateUrl: '/templates/landing.html'
          })
          .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
+         })
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/template/collection.html'   
          });
      }
 
