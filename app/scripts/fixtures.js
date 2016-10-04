2a55dea0fd1852ea34bedd2dcb9c5f50a180327b
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-10-04 17:59:24 -0500

    checkpoint-5

--- app/scripts/fixtures.js
+++ app/scripts/fixtures.js
@@ -13,18 +13,5 @@ var albumPicasso = {
     ]
 };
 
-var albumMarconi = {
-    name: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        { name: 'Hello, Operator?', length: '1:01' },
-        { name: 'Ring, ring, ring', length: '5:01' },
-        { name: 'Fits in your pocket', length: '3:21' },
-        { name: 'Can you hear me now?', length: '3:14' },
-        { name: 'Wrong phone number', length: '2:15' }
-    ]
-};
+
  
